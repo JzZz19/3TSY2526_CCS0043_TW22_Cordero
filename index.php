2bc3b50233ac720db8542d4d80df9443a28bd555
JzZz19 <josecords2006@gmail.com>
2026-04-20 12:01:37 +0800

    data adn time

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My php activities </title>
+</head>
+<body>
+    <div class="header">
+        <h1> these are my php activities]</h1>
+        <p>php is fun</p>
+    </div>
+</body>
+</html>
