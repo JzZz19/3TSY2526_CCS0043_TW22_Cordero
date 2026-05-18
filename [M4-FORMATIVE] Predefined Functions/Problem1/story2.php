8f2d6d879a069bc53036b7da6851476a94ae67f0
CordsTheIII <josecords2006@gmail.com>
2026-05-18 16:35:00 +0800

    Update story2.php

--- [M4-FORMATIVE] Predefined Functions/Problem1/story2.php
+++ [M4-FORMATIVE] Predefined Functions/Problem1/story2.php
@@ -1,13 +1,24 @@
+
 <?php require("header.php"); ?>
 
 <h3>Story 2</h3>
 
+<div class="story-container">
 
-<p><b>Name:</b> Maria Santos</p>
-<p><b>Birthday:</b> February 2, 2004</p>
-<p><b>Age:</b> 21</p>
-<p><b>Contact:</b> 09234567891</p>
+    <div class="story-info">
+        <p><b>Name:</b> De Guia, Jonash Aaron</p>
+        <p><b>Birthday:</b> December 13, 2005 </p>
+        <p><b>Age:</b> 20</p>
+        <p><b>Contact:</b> 0924766898</p>
 
-<p>Maria loves reading books and helping people.</p>
+        <p>Jonash is someone who always gives full effort in everything he does. Jonash enjoys working with others and is always ready to offer help when needed. Because of being reliable and considerate, Jonash is a great person to have as a classmate.
+</p>
+    </div>
+    
+    <div class="story-image">
+        <img src="P2.jpg" alt="Story 2 Image">
+    </div>
+
+</div>
 
 <?php require("footer.php"); ?>
