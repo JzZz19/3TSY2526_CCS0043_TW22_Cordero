c8c58a7eeae4c78049c6e61749dcd9741c19fd54
CordsTheIII <josecords2006@gmail.com>
2026-05-18 16:35:39 +0800

    Update story4.php

--- [M4-FORMATIVE] Predefined Functions/Problem1/story4.php
+++ [M4-FORMATIVE] Predefined Functions/Problem1/story4.php
@@ -1,14 +1,24 @@
+
 <?php require("header.php"); ?>
 
 <h3>Story 4</h3>
 
+<div class="story-container">
 
-<p><b>Name:</b> Ana Lopez</p>
-<p><b>Birthday:</b> April 4, 2003</p>
-<p><b>Age:</b> 22</p>
-<p><b>Contact:</b> 09456789123</p>
+    <div class="story-info">
+        <p><b>Name:</b> Reyes, Railey </p>
+        <p><b>Birthday:</b> June 15, 2005 </p>
+        <p><b>Age:</b> 20</p>
+        <p><b>Contact:</b> 09261794726</p>
 
-<p>Ana is creative and likes art and design.</p>
+        <p>Railey is a thoughtful and dedicated individual who always does best in every task. Railey enjoys helping classmates and works well with others. Railey is responsible and kind, making Railey a great person to be around.
+</p>
+    </div>
+    
+    <div class="story-image">
+        <img src="P4.jpg" alt="Story 4 Image">
+    </div>
 
+</div>
 
 <?php require("footer.php"); ?>
