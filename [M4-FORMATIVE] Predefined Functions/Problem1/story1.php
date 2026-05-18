fb87ffdf2be6c18047c109e7171a9d9a3914ebf0
CordsTheIII <josecords2006@gmail.com>
2026-05-18 16:34:47 +0800

    Update story1.php with new character details

--- [M4-FORMATIVE] Predefined Functions/Problem1/story1.php
+++ [M4-FORMATIVE] Predefined Functions/Problem1/story1.php
@@ -1,16 +1,23 @@
-//story1.php
 <?php require("header.php"); ?>
 
 <h3>Story 1</h3>
 
+<div class="story-container">
 
-<p><b>Name:</b> Juan Dela Cruz</p>
-<p><b>Birthday:</b> January 1, 2005</p>
-<p><b>Age:</b> 20</p>
-<p><b>Contact:</b> 09123456789</p>
+    <div class="story-info">
+        <p><b>Name:</b> Lising, Romar Paulo</p>
+        <p><b>Birthday:</b> July 24, 2006</p>
+        <p><b>Age:</b> 20</p>
+        <p><b>Contact:</b> 09198861738</p>
 
-<p>Juan is a kind and hardworking person who helps his classmates.</p>
+        <p>Romar is a dependable individual who always gives effort in everything he does. Romar enjoys cooperating with others and is always ready to lend support when needed. Romar’s positive attitude and dedication make him a valuable member of the class.
+</p>
+    </div>
+    
+    <div class="story-image">
+        <img src="P1.jpg" alt="Story 1 Image">
+    </div>
 
-
+</div>
 
 <?php require("footer.php"); ?>
