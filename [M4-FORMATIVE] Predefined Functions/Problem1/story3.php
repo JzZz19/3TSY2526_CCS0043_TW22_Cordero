fe41e00479cb6930f0a455ec65aaaf7130649199
CordsTheIII <josecords2006@gmail.com>
2026-05-18 16:35:14 +0800

    Update story3.php

--- [M4-FORMATIVE] Predefined Functions/Problem1/story3.php
+++ [M4-FORMATIVE] Predefined Functions/Problem1/story3.php
@@ -1,15 +1,24 @@
+
 <?php require("header.php"); ?>
 
 <h3>Story 3</h3>
 
+<div class="story-container">
 
-<p><b>Name:</b> Pedro Reyes</p>
-<p><b>Birthday:</b> March 3, 2006</p>
-<p><b>Age:</b> 19</p>
-<p><b>Contact:</b> 09345678912</p>
+    <div class="story-info">
+        <p><b>Name:</b> Alba, Aerick Lee </p>
+        <p><b>Birthday:</b> August 22, 2005</p>
+        <p><b>Age:</b> 20</p>
+        <p><b>Contact:</b> 09472668154</p>
 
-<p>Pedro enjoys playing basketball and studying.</p>
+        <p>Aerick is a dedicated and responsible individual who always puts effort into everything Aerick does. Aerick enjoys supporting classmates and working as part of a team. Aerick is reliable and respectful, making Aerick someone others can count on.
+</p>
+    </div>
+    
+    <div class="story-image">
+        <img src="P3.jpg" alt="Story 3 Image">
+    </div>
 
-
+</div>
 
 <?php require("footer.php"); ?>
